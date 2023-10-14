27cc479b35ff6f650392d819e0dec7a3ec089326
Elias K. Salem <48525998+EKenjiSalem@users.noreply.github.com>
2023-10-14 13:16:08 -0700

    Completed react intro, and began components blog post

--- src/components/react/Components.jsx
+++ src/components/react/Components.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './react.css';
+import reactfunccomp from '../../images/component-function.png';
+import reactfuncdynam from '../../images/component-dynamic.png';
+import reactclassfunc from '../../images/class-component.png';
+import resusecomp from '../../images/rsz_reusecomp.png';
 
 
 
@@ -15,6 +19,81 @@ const Components = () => {
          </div>
      
          <p className="react-main"> 
+          <h1 className="component-title"> What are Components? </h1>
+          In React, components are the building blocks of a user interface. They are reusable 
+          and self-contained pieces of code that represent a part of the user interface. Components
+          are the core concept in React and are used to create UI elements, manage the state of those 
+          elements, and define the behavior and structure of the application.
+<br />
+<br />
+        <strong> There are two main types of components in React: </strong>
+
+       <strong> Function Components: </strong> These are stateless, functional components that are defined as 
+        JavaScript functions. They are mainly used for simpler UI elements and don't have their
+         own internal state or lifecycle methods. Function components are highly encouraged in 
+         modern React development due to the introduction of hooks, which allow you to manage 
+         state and side effects in functional components.
+            <div className="jsx-img">
+              <img src={reactfunccomp} />
+            </div>
+
+<br />
+<br />  
+            <div className="jsx-img">
+              <img src={reactfuncdynam} />
+            </div>
+
+<br />
+<br />  
+       <strong> Class Components: </strong> These components are defined as JavaScript classes 
+       that extend React.Component. They offer more features, including the ability to manage 
+       internal state, lifecycle methods (such as componentDidMount and componentDidUpdate), 
+       and more complex logic.
+<br />
+<br />  
+       Class components also provide a set of lifecycle methods that allow you to perform actions
+       at different points in a component's lifecycle, such as when it's created, updated, or 
+       unmounted. These methods can be used for tasks like data fetching, setting up timers, or
+       cleaning up resources.
+       <div className="jsx-img">
+          <img src={reactclassfunc } />
+         </div>
+
+
+<br />
+<br />  
+
+        <h1 className="resuse-title"> Reusability and Composition </h1>
+        React components are designed to be reusable. This means you can define a component 
+        once and use it in multiple places within your application. For example, you could 
+        create a Header component and use it on various pages of your website.
+<br />
+<br /> 
+        This reusability makes your code more maintainable and reduces duplication.
+<br />
+<br /> 
+        Additionally, components can be composed by nesting them within one another. This allows 
+        you to build complex UI structures from simpler, smaller components. This concept of 
+        composition is one of the key advantages of React.
+
+<br />
+<br /> 
+
+        In this example, the ReusableComponent is a functional component that doesn't 
+        accept any props. It's a self-contained component that can be used as is, and it 
+        displays a consistent message and structure each time it's used.
+        <div className="jsx-img">
+          <img src={resusecomp} />
+         </div>
+
+  
+
+
+
+
+
+
+
 
 
             </p>
--- src/components/react/Syntax.jsx
+++ src/components/react/Syntax.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './react.css';
+import jsxbasic from '../../images/jsx-basic.png';
+import embedded from '../../images/embedexpress.png';
+import jsandjsx from '../../images/jsandjsx.png';
+import jsxattr from '../../images/Jsxattr.png';
 
 
 
 const Syntax = () => {
+
   return (
     <div className="js-post">
         <div className="js-post-wrapper">
@@ -15,12 +20,79 @@ const Syntax = () => {
          </div>
      
          <p className="react-main"> 
+          <h1 className="jsx-title"> What is JSX? </h1>
+         JSX stands for "JavaScript XML." It is a syntax extension for JavaScript often used
+         with React, a popular JavaScript library for building user interfaces. JSX allows
+         developers to write HTML-like code within their JavaScript files, making it easier 
+         to create and manipulate the structure of user interfaces in web applications.
+  <br />
+  <br />
+
+        <strong> Basic JSX Element: </strong>
+        We've defined a JSX element that represents an h1 heading with the
+        text "Hello, JSX!".
+         <div className="jsx-img">
+          <img src={jsxbasic} />
+         </div>
+         <div className="m-jsx-img">
+          <img src={jsxbasic} />
+         </div>
+  
+
+<br />
+<br />
+
+        <strong> Embedding Expressions: </strong>
+        You can embed JavaScript expressions within JSX using curly braces {}. 
+<br />
+<br />
+        In this case, the value of the name variable is dynamically inserted into the JSX 
+        element, resulting in "Hello, John!".
+        
+         <div className="jsx-img">
+          <img src={embedded} />
+         </div>
+<br />
+<br />
+        <strong> JSX and JavaScript: </strong>
+        JSX is not a separate language; it's an extension of JavaScript. You can use JavaScript
+        expressions and logic within JSX elements. Here's an example of rendering a list of items 
+        using JSX and JavaScript:
+        
+<br />
+<br />
+        In this example, we use the .map method to create a list of li elements from an array
+        of items. We also include a key attribute for each li to help React efficiently update
+        the DOM.
+        
+         <div className="jsx-img">
+          <img src={jsandjsx} />
+         </div>
+<br />
+<br />
+
+          <strong> JSX Attributes and Components: </strong>
+          In JSX, you can specify HTML attributes just like in regular HTML. Additionally,
+          you can create and use React components to encapsulate and reuse parts of your UI.
+<br />
+<br />
+          In this code, we define a Welcome component that takes a name prop. We then use this 
+          component within the App component to render personalized greetings.
+<br />
+<br />
+    
+
+        <div className="jsx-img">
+          <img src={jsxattr} />
+         </div>
+
+
 
 
             </p>
 
         </div>
-    </div>
+      </div>
   )
 }
 
