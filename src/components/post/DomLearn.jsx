ea52d808716f0104639b1b2fca10dbc9047dd593
Elias K. Salem <48525998+EKenjiSalem@users.noreply.github.com>
2023-11-20 12:22:14 -0800

    Completed multiple opinion based blog post

--- src/components/post/DomLearn.jsx
+++ src/components/post/DomLearn.jsx
@@ -5,7 +5,47 @@ import './blogcard.css';
 const DomLearn = () => {
   return (
     <div> 
-        <h1 className="master-web-title"> Dom Learn </h1>
+        <h1 className="master-web-title"> Should the DOM be taught 1st? </h1>
+
+        <p className="domlearn-main"> 
+        My coding journey started back late 2015. When I first started, I started with Codecademy.
+        spending a couple of months enaged in learning html, css, javascript. After that I had began
+        to learn on Udemy. I started with the Web Development bootcamp, upon completing that I was somewhat
+        impressed with udemy's inexpensive way to learn how to code, yet I was still 
+        a bit confused, yet still passionate about learning how to code. So I kept learning,
+        I enrolled in a couple more web developent programs and began to build my own projects.
+<br />
+<br />
+        After a few years of learning on udemy and evenn building a portfolio and multiple projects,
+        I applied for a couple jobs and got rejected, so I continued to engage into another coding
+        program Treehouse. At treehouse, I enrolled into the front-end web developent tech degree,
+        but dropped out, and just paid to use their coding langauge library which offered self paced programs.
+<br />
+<br />
+       Finally, I get to the point. All of the javascript programs had 1 think in common. The DOM was never
+       one of the first things to be taught. Know the Dom can a very complex process, but the DOM is the 
+       very thing connects HTML and CSS to Javascript. Noted there are inline methods that can be applied,
+       yet the DOM appears to be the preffered way to bring functionality to HTML and CSS.
+<br />
+<br />
+      When I first started, I remember learning about variables, data types, operators, loops, 
+      functions, if statements, and then the dom. In doing so, I recall that's why I was so confused.
+      Becuase, I had Learned all of these prgramming concepts without learning how this will apply 
+      to a web page. When I was first made aware of the DOM, I thought this was another programming 
+      concept. After months digging through, I realized that DOM is the very connector from 
+      prgramming to the browser.
+<br />
+<br />
+      With this new found logic, I opted to post the DOM as one of the top articles, to help 
+      someone who might run into the same issues. The DOM is the bridge between web page simplcity
+      and web page complexity. Really without the DOM, is web development still considered tech?
+      at that point HTML and CSS is really just web design. Web Design is awesome. The very
+      foundation of an enaging web experience, yet Javascript is that very thing that gives the
+      web page that 3d wow factor. So, thank you reading my rant, but I remain strong that the 
+      DOM should be one of the first taught in Javascript.
+<br />
+<br />
+        </p>
     </div>
   )
 }
